f4a7acc18dbf843977adfc4286fa23460c5ac646
raven <raven025@outlook.com>
2021-12-28 20:26:46 +0800

    add min

--- views_counter.js
+++ views_counter.js
@@ -4,50 +4,52 @@
  * This source code is licensed under the MIT License found in the
  * LICENSE file in the root directory of this source tree.
  */
-var ids = ["vc_page_views", "vc_site_views", "vc_page_users", "vc_site_users"];
-var server_url = undefined;
-ids.forEach(id => {
-    if($("#"+id).data("server-url")){
-    if(!server_url){
-        server_url = $("#"+id).data("server-url");
+ $(document).ready(function(){
+    var ids = ["vc_page_views", "vc_site_views", "vc_page_users", "vc_site_users"];
+    var server_url = undefined;
+    ids.forEach(id => {
+        if($("#"+id).data("server-url")){
+            if(!server_url){
+                server_url = $("#"+id).data("server-url");
+            }
+        }
+    })
+    function vc_hide(){
+        ids.forEach(id => {
+            $("#"+id).hide();
+        })
     }
+    function vc_text(res){
+        ids.forEach(id => {
+            $("#"+id).text(res[id]);
+        })
     }
+    function vc_show(){
+        ids.forEach(id => {
+            $("#"+id).show();
+        })
+    }
+    function vc_acquire(){
+        vc_hide();
+        $.ajax({
+            url: server_url,
+            type: "POST",
+            crossDomain: true,
+            data: {
+                src: window.location.href,
+            },
+            success: res => {
+            if(res["success"]){
+                vc_text(res);
+                vc_show();
+            }else{
+                console.log("Server error.")
+            }
+            },
+            error: (XMLHttpRequest, textStatus, errorThrown) => {
+                console.log(errorThrown);
+            }
+        })
+    }
+    vc_acquire();
 })
-function vc_hide(){
-    ids.forEach(id => {
-        $("#"+id).hide();
-    })
-}
-function vc_text(res){
-    ids.forEach(id => {
-        $("#"+id).text(res[id]);
-    })
-}
-function vc_show(){
-    ids.forEach(id => {
-        $("#"+id).show();
-    })
-}
-function vc_acquire(){
-    vc_hide();
-    $.ajax({
-        url: server_url,
-        type: "POST",
-        crossDomain: true,
-        data: {
-            src: window.location.href,
-        },
-        success: res => {
-        if(res["success"]){
-            vc_text(res);
-            vc_show();
-        }else{
-            console.log("Server error.")
-        }
-        },
-        error: (XMLHttpRequest, textStatus, errorThrown) => {
-            console.log(errorThrown);
-        }
-    })
-}
-vc_acquire();
