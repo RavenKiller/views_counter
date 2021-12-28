4080d343c7e1fa3087b17596d7f20bfb4044ce17
raven <raven025@outlook.com>
2021-12-28 09:30:59 +0800

    add views_counter.js

---
+++ views_counter.js
@@ -0,0 +1,40 @@
+var ids = ["vc_page_views", "vc_site_views", "vc_users"];
+var server_url = undefined;
+ids.forEach(id => {
+    if($("#"+id).data("server-url")){
+        server_url = $("#"+id).data("server-url");
+    }
+})
+function vc_hide(){
+    ids.forEach(id => {
+        $("#"+id).hide();
+    })
+}
+function vc_text(res){
+    ids.forEach(id => {
+        $("#"+id).text(res[id]);
+    })
+}
+function vc_show(){
+    ids.forEach(id => {
+        $("#"+id).show();
+    })
+}
+function vc_acquire(){
+    vc_hide();
+    $.ajax({
+        url: server_url,
+        type: "POST",
+        crossDomain: true,
+        data: {
+            src: window.location.href,
+        },
+        success: res => {
+            vc_text(res);
+            vc_show();
+        },
+        error: (XMLHttpRequest, textStatus, errorThrown) => {
+            console.log(errorThrown);
+        }
+    })
+}
